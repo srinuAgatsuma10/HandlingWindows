aadd9b5f01dbc857d1813cb5cb621315545d5e8b
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-23 10:13:16 +0530

    Orange HRM Setup

--- src/test/java/orangeHRM/HandlingWindows.java
+++ src/test/java/orangeHRM/HandlingWindows.java
@@ -1,5 +1,30 @@
 package orangeHRM;
 
-public class HandlingWindows {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class HandlingWindows {
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demo.automationtesting.in/Windows.html");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void handlinWindow() {
+		
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 }
