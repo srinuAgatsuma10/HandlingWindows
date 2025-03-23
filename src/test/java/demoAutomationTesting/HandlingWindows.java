420fd31665b0cf01b73a0e0f14648bfb76d8ed86
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-23 08:48:02 +0530

    Opening new Tab Window

--- src/test/java/demoAutomationTesting/HandlingWindows.java
+++ src/test/java/demoAutomationTesting/HandlingWindows.java
@@ -1,10 +1,14 @@
 package demoAutomationTesting;
 
 import java.time.Duration;
+import java.util.*;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class HandlingWindows {
 
@@ -21,7 +25,23 @@ public class HandlingWindows {
 	
 	@Test(priority=1)
 	public void openNewTabWindow() {
+		// Click
+		driver.findElement(By.xpath("//a[@href='http://www.selenium.dev']//button[@class='btn btn-info'][normalize-space()='click']")).click();
+		Set<String> tabs = driver.getWindowHandles();
+		System.out.println("Id's of current windows : "+tabs);
 		
+		// To access one by one convert into List
+		List<String> tabList = new ArrayList(tabs);
+		String parent = tabList.get(0);
+		String child = tabList.get(1);
+		
+		// Switching to Child Window
+		driver.switchTo().window(child);
+		System.out.println("This is Child Window : "+driver.getTitle());
+		
+		// Switching to Parent Window
+		driver.switchTo().window(parent);
+		System.out.println("This is Parent Window : "+driver.getTitle());
 	}
 	
 	@Test(priority=2)
