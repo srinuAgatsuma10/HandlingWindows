882d8c22134aa5aa619d958ec0a640f1413ac2b8
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-31 13:59:26 +0530

    new Tab Method

--- src/test/java/automationBlogSpot/HandlingWindows.java
+++ src/test/java/automationBlogSpot/HandlingWindows.java
@@ -1,8 +1,13 @@
 package automationBlogSpot;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -16,20 +21,37 @@ public class HandlingWindows {
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://demo.automationtesting.in/Windows.html");
+		driver.get("https://testautomationpractice.blogspot.com/");
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test
 	public void newTab() {
-		
+		// Click the button
+		driver.findElement(By.xpath("//button[normalize-space()='New Tab']")).click();
+
+		Set<String> windows = driver.getWindowHandles();
+
+		// To access one by one convert into List
+		List<String> tabList = new ArrayList(windows);
+		String parent = tabList.get(0);
+		String child = tabList.get(1);
+
+		// Switching to Child Window
+		driver.switchTo().window(child);
+		System.out.println("This is Child Window : " + driver.getTitle());
+		driver.close();
+
+		// Switching to Parent Window
+		driver.switchTo().window(parent);
+		System.out.println("This is Parent Window : " + driver.getTitle());
 	}
-	
+
 	@Test
 	public void popUpWindow() {
-		
+
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
