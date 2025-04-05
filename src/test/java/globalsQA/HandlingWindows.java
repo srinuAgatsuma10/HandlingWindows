9ad2bd935858faf6c23379498da6a5b933d72fd1
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-04-05 08:22:31 +0530

    newTabWindow Test Case

--- src/test/java/globalsQA/HandlingWindows.java
+++ src/test/java/globalsQA/HandlingWindows.java
@@ -1,7 +1,11 @@
 package globalsQA;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -23,7 +27,23 @@ public class HandlingWindows {
 
 	@Test
 	public void newTabWindow() {
+		driver.findElement(By.xpath(
+				"//div[@class='single_tab_div resp-tab-content resp-tab-content-active']//a[@class='button e.g. button_hilite button_pale small_button'][normalize-space()='Click Here']"))
+				.click();
+		Set<String> windowHandles = driver.getWindowHandles();
+		System.out.println("All Windows Id's :" + windowHandles);
 
+		List<String> winHandle = new ArrayList(windowHandles);
+		String parent = winHandle.get(0);
+		String child = winHandle.get(1);
+
+		// Switch to Child window
+		driver.switchTo().window(child);
+		System.out.println("This is Child Window : " + driver.getTitle());
+
+		// Switch to Parent window
+		driver.switchTo().window(parent);
+		System.out.println("This is Parent Window : " + driver.getTitle());
 	}
 
 	@AfterClass
