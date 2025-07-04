4f37d99e71bb7061f0bb4d61ccd1137014bb8163
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-04 10:22:08 +0530

    Open New Tab

--- src/test/java/tutorialsPoint/HandligWindows.java
+++ src/test/java/tutorialsPoint/HandligWindows.java
@@ -1,6 +1,9 @@
 package tutorialsPoint;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -24,15 +27,32 @@ public class HandligWindows {
 
 	@Test(priority = 1)
 	public void openNewTab() {
-
+		// Click New Tab button
+		driver.findElement(By.xpath("//button[normalize-space()='New Tab']")).click();
+		
+		Set<String> windows = driver.getWindowHandles();
+		
+		// Access all windows one by one
+		List<String> tabs = new ArrayList(windows);
+		String parent = tabs.get(0);
+		String child = tabs.get(1);
+		
+		// Switch to child window
+		driver.switchTo().window(child);
+		System.out.println("This is child window : " + driver.getTitle());
+		driver.close();
+		
+		// Switch to parent window
+		driver.switchTo().window(parent);
+		System.out.println("This is parent window : " + driver.getTitle());
 	}
 
-	@Test(priority = 2)
+//	@Test(priority = 2)
 	public void openNewWindow1() {
 
 	}
 
-	@Test(priority = 3)
+//	@Test(priority = 3)
 	public void openNewWindow2() {
 
 	}
